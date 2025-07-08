cf50172df13ed3ca178189812e534068b9f03601
fawazalisaeed.ahmedsyed <fawazalisaeed.ahmedsyed@shivaxintellipaatgmail.onmicrosoft.com@fawazVM.v5kioexahwfethba0cmukapgnc.phxx.internal.cloudapp.net>
2025-07-08 17:43:01 +0000

    updated

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorageacct"
+    resource_group_name  = "FPP-RG"
+    storage_account_name = "fawazpranaypranavaccname"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
