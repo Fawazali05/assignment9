6a09af4e3e19e5d6b6dfeede26250a3485c63aeb
Fawazali05 <60064268+Fawazali05@users.noreply.github.com>
2025-07-09 16:18:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "FPP-RG"
-    storage_account_name = "fawazpranaypranavaccname"
+    storage_account_name = "fawazpranaypranavacc"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
