723afee6e37c18a7f7914f976f6c5631f586598a
Fawazali05 <60064268+Fawazali05@users.noreply.github.com>
2025-07-10 17:47:55 +0530

    Update sql_server.tf

--- sql_server.tf
+++ sql_server.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mssql_server" "sql" {
   name                         = var.sql_server_name
   resource_group_name          = azurerm_resource_group.rg.name
-  location                     = var.location
+  location                     = "Central US"
   version                      = "12.0"
   administrator_login          = var.sql_admin
   administrator_login_password = var.sql_password
